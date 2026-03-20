a5925dad64cca82f42b30f7b33e9d3d963e20e4c
Nayan Tupe <nayantupe699@gmail.com>
2026-03-20 12:40:57 +0530

    Java Script ALL Conditions Test

---
+++ first.js
@@ -0,0 +1,83 @@
+// const student = {
+//     fullName: "Nayan Tupe",
+//     age: 21,
+//     cgpa: 8.2,
+//     iPass: true,
+// };
+
+// console.log(student)
+
+// const product = {
+//     title: "Ball Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+// };
+// console.log(product);
+
+
+// const profile = {
+//     username: "nayantupee",
+//     isFollow: false,
+//     followers: 123,
+//     following: 52,
+// }
+
+
+// console.log(typeof profile["username"]);  // console.log use for print output in website & this code using for personaly see obejct key type 
+
+// Artimatic Opertors
+// let a = 5;                         // ( = )  is the assignment opertor
+// let b = 7;
+// console.log("a + b =", a + b)
+// console.log("a - b =", a - b)
+// console.log("a * b =", a * b)
+// console.log("a / b =", a / b)   // Basic Artimatic opertors in js. opertors Symbols -> (+, -, /, *) 
+
+
+// console.log("a % b =", a % b)   // modulus opertors
+
+
+// console.log("a ** b =", a ** b)    // Exponentition opertors 
+
+// /**  unary opertors **/
+
+// console.log(" a-- =", a--)    // first value change then work
+// console.log(" a++ =", a++)    // first work then value after line change
+// console.log(" --a =", --a)     
+// console.log(" ++a =", ++a)    // first value change then print 
+
+/**  Assignment Opertors & Symbols ( = , += , -= , *= , %= , **=) **/
+
+
+// a += 4;  // a = a + 4
+// console.log( "a =   ", a)   // Value Change Perment 
+
+// /**  Comparison Opertors **/
+
+// console.log(" a == b", a == b)  // Equal to
+// console.log(" a != b", a != b)  // Not equal to 
+// console.log(" a === b", a === b) // Equal to & type   Strictily chack value data type then answer
+// console.log(" a > b", a > b)    // or ready (54) line me  change a value upper code that's why This is true in ouput 
+// console.log(" a < b", a < b)   
+// console.log(" a =", a )  
+
+/** Logical Opertors **/
+
+let a = 6;
+let b = 5;
+
+// let cond1 = a > b;   // true
+// let cond2 = a === 6; // true
+// console.log("cond1 && cond2", cond1 && cond2)     // (Logical AND want both condition true important then answer True)
+
+
+// Logical OR    only one condition important for OR opertor one codition True answer is true 
+
+// only one condition false answer      when both condition are false 
+let cond1 = a < b;   // false
+let cond2 = a === 6; // true
+console.log("cond1 || cond2", cond1 || cond2)   
+
+
+
