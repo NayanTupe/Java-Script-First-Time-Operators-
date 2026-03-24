61f76e5335d98e9e4f68cef8a53f74b837026068
subhashree275-cpu <subhashree275@gmail.com>
2026-03-24 15:34:52 +0530

    Practice Array Q1

--- first.js
+++ first.js
@@ -1,66 +1,21 @@
-// Strings
+// let friends = ["Nayan", "Roshan", "Rupesh", "Narayan", "Aniket", "Bhaven"]
 
-// let str = "Nayan Tupe";
-
-// console.log(str[6]); 
-
-// Templates Literals
-
-// let obj = {
-//     item: "Pen",
-//     price: 10,
-// };
-
-// let output = `the cost of ${obj.item} is ${obj.price} rupees`;
-// console.log(output);
-
-// console.log("the cost of", obj.item, "is", obj.price, "rupees");
-
-// let specialString = 'This is a template literal ';
-// console.log(typeof specialString);
-
-// console.log("Nayan\nTupe");    // \n use for string next line print 
-// console.log("Nayan\nTupe");       // \t use for stings space crete   like Nayan       Tupe this and string  space length calculate singal  
-
-
-// let str = "Nayan Tupe";
-// let reversed = "";
-// // console.log(str.length)
-// for (let i = str.length - 1; i >= 0; i--) {
-//     reversed += str[i];
-// }   
-
-// console.log(reversed); 
-
-// let str = "Nayan";
-
-// for (let i = 0; i < str.length; i++) {
-//     console.log(str[i]);
+// for(let i=0; i<friends.length; i++) {     // using loop in array 
+//     console.log(friends[i]);
 // }
 
-// String Methods in JS 
+/** Practice Q1*/
 
-// str.toUpperCase()
+let marks = [87,76,78,98,76,76,78];
 
-// let str = "NayanTupe"   //let A keyword in JavaScript Used to declare (create) a variable
-// let newStr = str.toUpperCase(); //  newStr create beacause of orginal string variable not change (immatable)That's why new varibable create let newStr Creating another variable called newStr
-// console.log(newStr);
+let sum = 0;
 
-let str = "     Nayan  Tupe    21    "
-console.log(str.trim());    // Remove all space starting and last using .trim()
+for (let val of marks) {
+    sum += val;
+}
 
-let x = "0123456789";
-console.log(x.slice(2, 5));    // string part return using .slice()
-
-
-let str1 = "Nayan"
-let str2 = "Tupe"
-let res = str1.concat(str2);    //concat use for 2 other variable string mix like output :- NayanTupe 
-console.log(res);
+let avg = sum / marks.length;
+console.log(`avg marks of the class = ${avg}`);
 
 
-let fullName = prompt("enter your fullname without space");
 
-let username = "@" + fullName + fullName.length;
-
-console.log(username);
