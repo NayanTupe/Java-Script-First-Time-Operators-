c55e10e270a533145accd2673e1e8ceb1fe887aa
subhashree275-cpu <subhashree275@gmail.com>
2026-03-25 17:55:26 +0530

    Noraml Dom function use .forEach

--- first.js
+++ first.js
@@ -1,15 +1,10 @@
-var bulb = document.querySelector("#bulb");
-var btn = document.querySelector("button");
+var h = document.querySelectorAll("h1"); // All Select h1 tag to help (.querySelectorAll) and single (1) select h1 tag to help (.querySelector)
+console.log(h); // h means variable name to defined upper line
 
-var flag = 0;
+h.forEach(function (e) {
+  // forEach is a loop method || It runs for each item in array
+  // (e) is a callback function || e = each element (one by one)
+  // e फक्त एक variable नाव आहे // तो current element दर्शवतो // तुम्ही काहीही नाव वापरू शकता:
 
-btn.addEventListener("click", function () {
-  // case-sensitive language
-  if (flag == 0) {
-    bulb.style.backgroundColor = "yellow";
-    flag = 1;
-  } else {
-    bulb.style.backgroundColor = "transparent";
-    flag = 0;
-  }
+  console.log(e);
 });
